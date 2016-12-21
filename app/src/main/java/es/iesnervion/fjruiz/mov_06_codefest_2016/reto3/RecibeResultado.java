bc1bfb244eb88ad31424f77ef4c88e9a374d0801
fjruiz <pajarrurro@gmail.com>
2016-12-21 09:07:57 +0100

    librerias

--- app/src/main/java/es/iesnervion/fjruiz/mov_06_codefest_2016/reto3/RecibeResultado.java
+++ app/src/main/java/es/iesnervion/fjruiz/mov_06_codefest_2016/reto3/RecibeResultado.java
@@ -1,8 +1,5 @@
 package es.iesnervion.fjruiz.mov_06_codefest_2016.reto3;
 
-/**
- * Created by fjruiz on 30/11/16.
- */
 
 public interface RecibeResultado {
     public void onResultReceived(Integer resultado);
